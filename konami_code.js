cf574bdf2f03a1239e89769373b0001a3182595f
brandon corey <bcorey85@gmail.com>
2019-03-31 18:09:36 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,14 +1,26 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+// const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+const codes = [
+  "ArrowUp",
+  "ArrowUp",
+  "ArrowDown",
+  "ArrowDown",
+  "ArrowLeft",
+  "ArrowRight",
+  "ArrowLeft",
+  "ArrowRight",
+  "b",
+  "a"
+];
 
 
 function init(){
   var index = 0
   document.body.addEventListener("keydown",function(e){
       var key = parseInt(e.detail)
-      if(key === code[index]){
+      if(key === codes[index]){
         index++
-        if(index === code.length - 1){
+        if(index === codes.length - 1){
           alert("YOU DID IT")
           index = 0
         }

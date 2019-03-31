d080c712b558bc9f1fa13a6d362d49cd14cdba75
brandon corey <bcorey85@gmail.com>
2019-03-31 18:11:49 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,19 +1,5 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-// const codes = [
-//   "ArrowUp",
-//   "ArrowUp",
-//   "ArrowDown",
-//   "ArrowDown",
-//   "ArrowLeft",
-//   "ArrowRight",
-//   "ArrowLeft",
-//   "ArrowRight",
-//   "b",
-//   "a"
-// ];
-
-
 function init(){
   var index = 0
   document.body.addEventListener("keydown",function(e){
